6f0d9d85203c38e232c44b28d89da3348537ee20
Shefaul-22 <cse1805022brur@gmail.com>
2025-12-03 12:16:45 +0600

    Redeploy server after fixing issues

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/', (req, res) => {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const db = client.db('rentwheels-db');
         const usersCollection = db.collection('users')
